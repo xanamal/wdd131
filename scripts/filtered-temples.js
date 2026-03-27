eee198f59c59d67b2c4711fe7956ec2331dd7676
xanamal <xanenthegreat@gmail.com>
2026-03-26 19:10:05 -0700

    changed code

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,21 +1,3 @@
-const hamButton = document.querySelector("#hamburgerMenu");
-const navigation = document.querySelector(".menulist");
-const menuItems = document.querySelectorAll(".menuitem");
-const pageTitle = document.querySelector("h1");
-
-hamButton.addEventListener("click", () => {
-	navigation.classList.toggle("open");
-	hamButton.classList.toggle("open");
-});
-menuItems.forEach(menuItem => {
-    menuItem.addEventListener("click", () => {
-        const currentActive = document.querySelector(".active");
-        currentActive.classList.toggle("active");
-        menuItem.classList.toggle("active");
-        pageTitle.textContent = menuItem.textContent;
-    });
-});
-
 
 
 const temples = [
